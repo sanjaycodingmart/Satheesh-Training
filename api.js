820e4444472ebae77dd444d921f200bbd35acc52
Satheeshkumar99 <123satheesh99@gmail.com>
2020-01-07 12:48:45 +0530

    bigbucket task

--- api.js
+++
@@ -1,3 +0,0 @@
-module.exports = {
-    key: 'OOe2zKB3kTo7W86r2X4b4IstCcUq1pHp'
-}
--- local.js
+++
@@ -1,6 +0,0 @@
-
-function apicall(){
-  let tring='OOe2zKB3kTo7W86r2X4b4IstCcUq1pHp';
-    return tring;
-}
-    
